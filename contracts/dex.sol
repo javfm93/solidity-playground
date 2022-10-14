45912d8011a485e0b0a7675de4400d7416848d0d
Javier Vazquez <javier.vazquez@thoughtworks.com>
2022-10-14 18:49:16 +0200

    changes

--- contracts/dex.sol
+++ contracts/dex.sol
@@ -4,6 +4,8 @@ pragma experimental ABIEncoderV2;
 import "./Wallet.sol";
 
 contract Dex is Wallet {
+    using SafeMath for uint256;
+
     enum OrderType {
         BUY,
         SELL
@@ -12,20 +14,49 @@ contract Dex is Wallet {
     struct Order {
         uint id;
         address trader;
-        bool isBuyOrder;
+        OrderType orderType;
         bytes32 ticker;
         uint amount;
         uint price;
     }
 
-    mapping(bytes32 => mapping(OrderType => Order[])) orderBook;
+    uint public nextOrderId = 0;
+    // what have bigger gas fees, mapping of a mapping or independant mappings?
+    // is a public variable modifiable from outside?
+    mapping(bytes32 => Order[]) public buyOrderBook;
+    // mapping(bytes32 => Order[]) public sellOrderBook;
+    function createBuyLimitOrder(bytes32 ticker, uint256 amount, uint256 price) public {
+        require(balances[msg.sender]["ETH"] >= amount.mul(price), "Not enough ETH balance to buy");
+        Order[] storage orders = buyOrderBook[ticker];
+        orders.push(Order(nextOrderId++, msg.sender, OrderType.BUY, ticker, amount, price));
 
-    function getOrderBook(bytes32 ticker, OrderType orderType) public view returns(Order[] memory){
-        return orderBook[ticker][orderType];
+        uint i = orders.length > 0 ? orders.length - 1 : 0;
+        while(i > 0){
+            if(orders[i - 1].price > orders[i].price) {
+                break;
+            }
+            Order memory orderToMove = orders[i - 1];
+            orders[i - 1] = orders[i];
+            orders[i] = orderToMove;
+            i--;
+        }
     }
 
-    // function createLimitOrder() public {
+    // function createSellLimitOrder(bytes32 ticker, uint256 amount, uint256 price) public {
+    //     require(balances[msg.sender][ticker] >= amount, "Not enough tokens to sell");
 
+    //     Order[] storage orders = orderBook[ticker][OrderType.SELL];
+    //     orders.push(Order(nextOrderId++, msg.sender, OrderType.SELL, ticker, amount, price));
+
+    //     uint i = orders.length > 0 ? orders.length - 1 : 0;
+    //     while(i > 0){
+    //         if(orders[i - 1].price < orders[i].price) {
+    //             break;
+    //         }
+    //         Order memory orderToMove = orders[i - 1];
+    //         orders[i - 1] = orders[i];
+    //         orders[i] = orderToMove;
+    //         i--;
+    //     }
     // }
-
 }
